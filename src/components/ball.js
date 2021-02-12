7bd61c6242547821a947ce8b3c9e3ce98032276c
Max Yuan <maxyuan6717@gmail.com>
2021-02-12 03:58:08 -0500

    Text changes on click

--- src/components/ball.js
+++ src/components/ball.js
@@ -3,6 +3,8 @@ import styles from "./ball.module.css";
 
 const Ball = () => {
   const [shake, setShake] = useState(false);
+  const [indx, setIndx] = useState(0);
+  const texts = ["Test Text 1", "Test Text 2", "Test Text 3", "Test Text 4"];
   return (
     <>
       {/* <section className={styles.stage}>
@@ -11,18 +13,21 @@ const Ball = () => {
         </figure>
       </section> */}
       <div
-        className={"mx-auto d-flex " + (shake ? styles.move : "")}
+        className={`m-auto d-flex ${styles.container} ${
+          shake ? styles.move : ""
+        }`}
         onClick={() => {
           setShake(true);
           setTimeout(() => {
             setShake(false);
+            setIndx(indx + 1);
           }, 1000);
         }}
       >
         <figure className={styles.circle} />
         <figure className={styles.glass} />
         <figure className={styles.triangle} />
-        <span className={styles.text}>Testing text</span>
+        <span className={styles.text}>{texts[indx]}</span>
       </div>
     </>
   );
