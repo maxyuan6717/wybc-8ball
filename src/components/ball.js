445295acbeeeb51634fd825e3e297506ca8e4ff7
Max Yuan <maxyuan6717@gmail.com>
2021-02-12 14:39:58 -0500

    switch to 8ball image

--- src/components/ball.js
+++ src/components/ball.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import styles from "./ball.module.css";
+import eightball from "../8ball.png";
 
 const Ball = () => {
   const [shake, setShake] = useState(false);
   const [indx, setIndx] = useState(0);
   const texts = ["Test Text 1", "Test Text 2", "Test Text 3", "Test Text 4"];
+  const size = 600;
   return (
     <>
-      {/* <section className={styles.stage}>
-        <figure className={styles.ball}>
-          <span className={styles.shadow} />
-        </figure>
-      </section> */}
       <div
         className={`m-auto d-flex ${styles.container} ${
           shake ? styles.move : ""
@@ -20,13 +17,20 @@ const Ball = () => {
           setShake(true);
           setTimeout(() => {
             setShake(false);
-            setIndx(indx + 1);
+            setIndx((indx + 1) % texts.length);
           }, 1000);
         }}
       >
-        <figure className={styles.circle} />
-        <figure className={styles.glass} />
-        <figure className={styles.triangle} />
+        {/* <section className={styles.stage}>
+          <figure className={styles.ball}>
+            <span className={styles.shadow} />
+            <span className={styles.eight} />
+          </figure>
+    </section> */}
+        {/* <figure className={styles.circle} /> */}
+        {/* <figure className={styles.glass} />
+        <figure className={styles.triangle} /> */}
+        <img src={eightball} width={size} height={size} />
         <span className={styles.text}>{texts[indx]}</span>
       </div>
     </>
