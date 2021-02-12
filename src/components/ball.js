23f81f60f3ba530a279d45312b12f7cf06860b78
Max Yuan <maxyuan6717@gmail.com>
2021-02-12 03:52:44 -0500

    Ball shakes on click

--- src/components/ball.js
+++ src/components/ball.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ball.module.css";
 
 const Ball = () => {
+  const [shake, setShake] = useState(false);
   return (
     <>
-      <section className={styles.stage}>
+      {/* <section className={styles.stage}>
         <figure className={styles.ball}>
           <span className={styles.shadow} />
         </figure>
-      </section>
-      <figure className={styles.glass} />
-      <figure className={styles.triangle} />
+      </section> */}
+      <div
+        className={"mx-auto d-flex " + (shake ? styles.move : "")}
+        onClick={() => {
+          setShake(true);
+          setTimeout(() => {
+            setShake(false);
+          }, 1000);
+        }}
+      >
+        <figure className={styles.circle} />
+        <figure className={styles.glass} />
+        <figure className={styles.triangle} />
+        <span className={styles.text}>Testing text</span>
+      </div>
     </>
   );
 };
